36ed7971227c3a69b8c5314de0ac27eacd7c1ff8
Shanley Caswell <shanleycaswell@gmail.com>
2023-07-25 10:04:49 -0400

    all tests passing

--- server/models.py
+++ server/models.py
@@ -7,17 +7,42 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+
+
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+
+    animals = db.relationship('Animal', backref='zookeeper')
+
+    def __repr__(self):
+        return f'<Zookeeper name: {self.name}>'
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.relationship('Animal', backref='enclosure')
+
+    def __repr__(self):
+        return f'<Enclosure Environment {self.environment}>'
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    def __repr__(self):
+        return f'<Animal name {self.name}, species {self.species}>'
+
